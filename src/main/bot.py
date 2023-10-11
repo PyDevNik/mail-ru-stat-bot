5636f6a823c910fa1b5e17762e88cf7cfbce75a0
PyDevNik <107363471+PyDevNik@users.noreply.github.com>
2023-10-11 19:19:31 +0300

    Update bot.py

--- src/main/bot.py
+++ src/main/bot.py
@@ -5,32 +5,27 @@ import messages as msgs
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.utils import executor
-
+from os import getenv
 from domain_parser import create_stats_table
 
-with open('configuration.json') as config_file:
-    config = json.load(config_file)
-    REFRESH_TOKEN = config['refresh_token']
-    TOKEN = config['bot_token']
-    ADMINS = config['admins']
-    DOMAIN = config['domain']
+REFRESH_TOKEN = os.getenv('refresh_token')
+TOKEN = os.getenv('bot_token')
+ADMINS = ["nikitosPy"]
+DOMAIN = "habr.com"
 
-bot = Bot(TOKEN, parse_mode = "MarkdownV2")
+bot = Bot(TOKEN)
 dp = Dispatcher(bot)
-
 
 @dp.message_handler(commands=['start'])
 async def start(msg: Message):
     if msg.from_user.username in ADMINS:
-        await msg.answer(msgs.HELLO_MESSAGE.format(DOMAIN), parse_mode = "")
-
+        await msg.answer(msgs.HELLO_MESSAGE.format(DOMAIN))
 
 @dp.message_handler(commands=['get_stat'])
 async def get_statistic(msg: Message):
     if msg.from_user.username in ADMINS:
         print(f'@{msg.from_user.username}')
         data = create_stats_table(REFRESH_TOKEN, DOMAIN)
-
         if not isinstance(data[0], type(None)):
             with open('result.xlsx', 'rb') as table_result:
                 await msg.answer_document(table_result)
